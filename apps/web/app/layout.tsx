9719cbb1681abca7d5c8353893aa2e4e1e4b7153
bryancalvarenga <bryancalvarenga@outlook.com.br>
2026-03-18 04:28:01 -0300

    add: icon.png

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -26,20 +26,7 @@ export const metadata: Metadata = {
     "reflexão",
   ],
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
+    icon: "icon.png",
     apple: "/apple-icon.png",
   },
 };
